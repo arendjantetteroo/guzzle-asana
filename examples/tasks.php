be4deaa98286c5f8bdc152da301224e7c12c51e0
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2013-04-16 14:05:24 +0200

    Add creation of subtasks

--- examples/tasks.php
+++ examples/tasks.php
@@ -58,9 +58,34 @@ $updatedtask = $asana_client->updateTask(
 );
 print_r($updatedtask);
 
+// Create a subtask
+print "Create a subtask for this task $taskid\n";
+$subtask = $asana_client->createSubTask(
+	array(
+		'assignee'  	  => 'me',
+		'assignee_status' => 'upcoming', 		// One off inbox, later, today or upcoming
+		'name' 			  => 'Asana api test subtask',
+		'completed' 	  => 0, 						// 0 for false, 1 for true
+		'notes' 		  => "testing notes for subtask",
+		'workspace' 	  => $workspace_id,
+		'due_on' 		  => $duedate->format('Y-m-d'),   
+		'followers' 	  => array(),
+		'parent-id'       => $taskid
+	)
+);
+print_r($subtask);
+// 
+
 // Get subtasks for this task
+print "Get subtasks\n";
 $subtask = $asana_client->getSubTasks(array('task-id' => $taskid));
 print_r($subtask);
+
+print "Delete this subtask with a normal deleteTask\n";
+$data = $asana_client->deleteTask(array('task-id' => $subtask['id']));
+if(count($data) == 0){
+	print "Subtask deleted\n";
+}
 
 // Remove the created task
 print "deleteTask\n";
